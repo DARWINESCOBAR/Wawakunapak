db6014517f32e7f942ed6524a2b248db74b73a48
AngeloDavid <angelodavid95@hotmail.es>
2018-09-03 22:09:16 -0500

    cambio de bienvenido y nuevo logo

--- appWawakunapak/src/app/app.component.ts
+++ appWawakunapak/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      timer(6000).subscribe(() => this.showSplash = false) ;
+      timer(5000).subscribe(() => this.showSplash = false) ;
 
 
     });
